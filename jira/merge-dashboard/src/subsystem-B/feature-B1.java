1a94d3db1aebd1967fe8e556ade5e1e78fb1d6dc
rataylor <rataylor@extremenetworks.com>
2022-06-06 22:19:38 -0700

    Adding first two feature B files

---
+++ jira/merge-dashboard/src/subsystem-B/feature-B1.java
@@ -0,0 +1,6 @@
+// file implementing feature B
+class featureB1{
+	public static void featureB1() {
+		System.out.println("exercising featureB1");
+	}
+}
---
+++ jira/merge-dashboard/src/subsystem-B/feature-B2.java
@@ -0,0 +1,6 @@
+// file implementing feature B
+class featureB2{
+	public static void featureB2() {
+		System.out.println("exercising featureB2");
+	}
+}
