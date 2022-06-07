8ea6fae45ef2c3021d5cc170f4f8b736a67fa43d
rataylor <rataylor@extremenetworks.com>
2022-06-06 22:11:52 -0700

    Adding another file

--- jira/merge-dashboard/src/featureA2.java
+++ jira/merge-dashboard/src/featureA2.java
@@ -1,6 +1,6 @@
-// file implementing feature A
-class featureA{
-	public static void featureA() {
-		System.out.println("exercising featureA");
+// file implementing feature A2
+class featureA2{
+	public static void featureA2() {
+		System.out.println("exercising featureA2");
 	}
 }
